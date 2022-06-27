2076d01c9674b014a5d869c69fd57a7b5d5f064a
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2022-06-27 21:25:26 +0600

    changes

--- server/index.js
+++ server/index.js
@@ -71,3 +71,47 @@ app.get('/read', async(req, res) => {
 app.listen(3002, () => {
     console.log("Server Running at PORT 3002")
 })
+
+// const express = require('express');
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// javascriptObj = {name: "John", age: 24}
+// jsonObj = {"name" : "John", "age": 24}
+
+// app.post('/contact', (req, res) => {
+//     if (!req.body.name) {
+//         return res.status(400).send('Name is required');
+//     }
+//     // Database stuff
+//     res.status(201).send(`Thank you ${req.body.name}`);
+// });
+
+// app.post('/login', (req, res) => {
+//     if (!req.header('x-auth-token')) {
+//         return res.status(400).send('No Token');
+//     }
+//     if (req.header('x-auth-token') !== '123456') {
+//         return res.status(401).send('Not authorized');
+//     }
+//     res.send('Logged in');
+// })
+
+// app.put('/post/:id', (req, res) => {
+//     // DATABASE STUFF
+//     res.json({
+//         id: req.params.id,
+//         title: req.body.title
+//     });
+// });
+
+// app.delete('/post/:id', (req, res) => {
+//     // DATABASE STUFF
+//     res.json({ msg: `Post ${req.params.id} deleted` });
+// });
+
+// app.listen(3004, () => {
+//     console.log(`Server started on 3004`);
+// })
